4748dd3dfc15180c91e695d9d43253a366e015bc
qiulin <qlin.dev@outlook.com>
2025-03-19 22:00:47 +0800

    update: convert.MapTo

---
+++ convert/struct.go
@@ -0,0 +1,16 @@
+package convert
+
+import "github.com/mitchellh/mapstructure"
+
+func MapTo(in map[string]any, out interface{}) error {
+	config := mapstructure.DecoderConfig{
+		WeaklyTypedInput: true,
+		TagName:          "json",
+		Result:           out,
+	}
+	decoder, err := mapstructure.NewDecoder(&config)
+	if err != nil {
+		return err
+	}
+	return decoder.Decode(in)
+}
