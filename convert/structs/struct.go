1bdec05de88d621ef44845fccd07894a012dcd09
Qiulin Li <liqiulin@outlook.com>
2025-11-07 11:27:35 +0800

    update

--- convert/structs/struct.go
+++ convert/structs/struct.go
@@ -1,16 +1,24 @@
 package structs
 
-import "github.com/mitchellh/mapstructure"
+import (
+	"github.com/go-viper/mapstructure/v2"
+)
 
-func MapTo(in map[string]any, out interface{}) error {
+func MapTo(in map[string]any, out interface{}, opts ...Option) error {
 	config := mapstructure.DecoderConfig{
 		WeaklyTypedInput: true,
 		TagName:          "json",
 		Result:           out,
 	}
+	for _, o := range opts {
+		o(&config)
+	}
+
 	decoder, err := mapstructure.NewDecoder(&config)
 	if err != nil {
 		return err
 	}
 	return decoder.Decode(in)
 }
+
+type Option func(config *mapstructure.DecoderConfig)
